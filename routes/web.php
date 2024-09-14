a5daa6e78185f532b36fd66c1035fde8cd027734
TSMMONKEY <70806953+TSMMONKEY@users.noreply.github.com>
2024-09-14 13:08:49 +0200

    added the products page, home page and the single service page. made the page controller and the model

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+// Single Product
+Route::get('/single-product', function () {
+    return view('pages.single-product');
+})->name('product.name');
+
+// All Products
+Route::get('/products', function () {
+    return view('pages.products');
+})->name('products');
